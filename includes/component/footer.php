f08cc268360b14b38d74261d92a64296ff438c8a
eleqtrasoft <77853966+eleqtrasoft@users.noreply.github.com>
2022-04-02 17:17:37 +0530

    Add files via upload

---
+++ includes/component/footer.php
@@ -0,0 +1,76 @@
+<footer class="container-fluid  pt-5 pb-3" id= "footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4 col-lg-3 pb-5">
+
+                    <!-- Brand -->
+                    <img src="images/logo/text_logo.svg" alt="text-white logo" class="footer_logo img-fluid mb-2">
+                    <small class="d-block mb-3 text-white-50">© 2020 Eleqtarsoft. All right reserved.</small>
+
+
+                    <!-- Social -->
+                    <ul class="list-unstyled list-group list-group-horizontal list-social mb-6 mb-md-0">
+                        <li class="list-inline-item list-social-item">
+                            <a href="http://instagram.com/eleqtrasoft" class="text-decoration-none">
+                            <i class="fa fa-instagram fa-2x social-icon" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item list-social-item">
+                            <a href="https://facebook.com/eleqtrasoft" class="text-decoration-none">
+                            <i class="fa fa-facebook-square fa-2x social-icon" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item list-social-item">
+                            <a href="http://youtube.com/channel/UCIRqTNeAD6A-D9q2lBHQrnA" class="text-decoration-none">
+                            <i class="fa fa-youtube-play fa-2x social-icon" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item list-social-item">
+                            <a href="https://www.behance.net/eleqtrasoft" class="text-decoration-none">
+                            <i class="fa fa-behance-square fa-2x social-icon" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="col-6 col-md" id="social-media">
+                    <h5 class="text-white">Socail Media</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-white-50" href="https://facebook.com/eleqtrasoft">Facebook</a></li>
+                        <li><a class="text-white-50" href="http://instagram.com/eleqtrasoft">Instagram</a></li>
+                        <li><a class="text-white-50" href="https://www.behance.net/eleqtrasoft">Behance</a></li>
+                        <li><a class="text-white-50" href="http://youtube.com/channel/UCIRqTNeAD6A-D9q2lBHQrnA">Youtube</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="text-white">Courses</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-white-50" href="#">Web Development</a></li>
+                        <li><a class="text-white-50" href="#">App Development</a></li>
+                        <li><a class="text-white-50" href="#">Film & Animation</a></li>
+                        <li><a class="text-white-50" href="#">Robotics & Automation</a></li>
+                        <li><a class="text-white-50" href="#">Graphic Designing</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="text-white">Resources</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-white-50 link-disabled" href="#">Business</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Education</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Documentation</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="text-white">About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-white-50 link-disabled" href="#">Team</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Our Vision/Mission</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Gallery</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Terms</a></li>
+                        <li><a class="text-white-50 link-disabled" href="#">Privacy & Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ includes/component/header.php
@@ -0,0 +1,35 @@
+<header class="sticky-top">
+ <nav class="navbar myshadow navbar-expand-lg navbar-light bg-light justify-content-sm-start" >
+      <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+            <img src="images/logo/logo.svg" width="30" alt="logo"> <img class="header-logo" src="images/logo/text_logo.svg"
+             height="25" alt="test logo" style="border-left: 2px solid grey; padding-left: 2%;">
+        </a>
+        <button class="navbar-toggler align-self-start" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse bg-light p-3 p-lg-0 mt-5 mt-lg-0 d-flex flex-column flex-lg-row flex-xl-row justify-content-lg-end mobileMenu on-right"
+          id="navbarSupportedContent"
+        >
+          <ul class="navbar-nav align-self-stretch text-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="#home"
+                >Home <span class="sr-only">(current)</span></a
+              >
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#courses">Courses</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact-us">Contact us</a>
+            </li>
+            
+        </div>
+      </div>
+    </nav>
+    <div class="overlay"></div>
+</header>
